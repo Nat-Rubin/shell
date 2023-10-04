8d4b94e4bab8312eacf2092cb3a2f32caa48ab72
Nat-Rubin <NathanielRubinM@gmail.com>
2023-10-04 08:50:37 -0400

    have commands and ||/&& functionality

--- src/shell.rs
+++ src/shell.rs
@@ -1,3 +1,267 @@
+extern crate dirs;
+
+use std::path::{Path};
+use std::env;
+use std::fs::File;
+use std::io::{stdin, stdout, Write, BufRead, BufReader, Read};
+use std::process::{Command};
+use ansi_term::{Color, Style};
+
+// Global Constants
+const MAX_HISTORY:usize = 10;
+const MAX_JOBS:usize = 255;
+
+struct HistoryStruct {
+    history: [String; MAX_HISTORY],
+    history_count: usize,
+}
+
+impl HistoryStruct {
+    fn new() -> Self {
+        HistoryStruct {
+            history: std::array::from_fn(|_| String::new()),
+            history_count: 0,
+        }
+    }
+
+    fn add_to_history(&mut self, line: &String) {
+        if self.history_count == MAX_HISTORY {
+
+        } else {
+            self.history[self.history_count] = line.clone();
+            self.increment_history_count();
+        }
+    }
+    fn increment_history_count(&mut self) {
+        self.history_count += 1;
+    }
+
+    fn print_history(&self) {
+        for i in 0..self.history_count {
+            println!("{} {}", i+1, self.history[i]);
+        }
+    }
+}
+
+struct JobStruct {
+    jobs: [String; MAX_JOBS],
+    job_ids: [i32; MAX_JOBS],
+    job_pids: [i32; MAX_JOBS],
+    current_job_id: i32,
+}
+
+impl JobStruct {
+    fn new() -> Self {
+        JobStruct {
+            jobs: std::array::from_fn(|_| String::new()),
+            job_ids: std::array::from_fn(|_| -1),
+            job_pids: std::array::from_fn(|_| -1),
+            current_job_id: 0,
+        }
+    }
+}
+
+// TODO: implement this somehow
+struct SettingsStruct {
+    font: String,
+    font_size: u32,
+}
+
+impl SettingsStruct {
+    fn new() -> Self {
+        SettingsStruct {
+            font: String::new(),
+            font_size: SettingsStruct::get_current_font_size(),
+        }
+    }
+
+    fn get_current_font_size() -> u32 {
+        // match env::consts::OS {
+        //     "linux" => println!("Running on Linux"),
+        //     "macos" => println!("Running on macOS"),
+        //     "windows" => println!("Running on Windows"),
+        //     _ => println!("Running on an unknown OS"),
+        // }
+        return 0;
+    }
+}
+
+struct ShellStruct {
+    history_struct: HistoryStruct,
+    job_struct: JobStruct,
+    settings_struct: SettingsStruct,
+}
+
+impl ShellStruct {
+    fn new() -> Self {
+        ShellStruct {
+            history_struct: HistoryStruct::new(),
+            job_struct: JobStruct::new(),
+            settings_struct: SettingsStruct::new(),
+        }
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Shell!");
+
+    let mut shell_struct: ShellStruct = ShellStruct::new();
+
+    let home_dir = dirs::home_dir().unwrap();
+    let _=env::set_current_dir(&home_dir.as_path());
+
+    loop {
+        print_pwd();
+
+        // get and parse input
+        let mut input = String::new();
+        let _=stdout().flush();
+        stdin().read_line(&mut input).unwrap();
+        input.pop();  // remove \n
+
+        if input == "" { continue; }
+
+        shell_struct.history_struct.add_to_history(&input);
+
+        let mut tokens: Vec<&str> = input.split(" ").collect();
+        tokens.retain(|&char| char != "");  // get rid of extra spaces
+
+        while !tokens.is_empty() {  // loop while && or || still exists
+            let (mut tokens_part, separator) = split_input(&mut tokens);
+
+            let command: String = tokens_part[0].to_lowercase();
+            tokens_part.remove(0);  // remove the command from the tokens vec
+
+            // run command and check if it exists, continue if true
+            let result: bool = execmd(&shell_struct, &command, &tokens_part);
+            if result { continue }
+
+            // fork and run
+            let mut cmd = Command::new(command.as_str()).args(&tokens_part).status();
+            match cmd {
+                Ok(res) => {
+                    if separator.is_some() {
+                        let separator_copy = separator.unwrap().clone();
+                        if separator_copy == "||" {
+                            if cmd.unwrap().success() {
+                                break;
+                            }
+                        } else if separator_copy == "&&" {
+                            if cmd.unwrap().success() {
+                                continue;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                },
+                Err(_) => {
+                    println!("Command {command} not recognized");
+
+                    if separator.is_some() && separator.unwrap() == "&&" {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+/**
+    Checks the command and calls the proper function
+    Returns true if command is recognized, otherwise false
+ */
+fn execmd(shell_struct: &ShellStruct, command: &String, args: &Vec<&str>) -> bool {
+    let command_str: &str = command.as_str();
+    match command_str {
+        "cd"      => cd(args),
+        "cat"     => cat(args),
+        "history" => shell_struct.history_struct.print_history(),
+        _         => return false,
+    };
+    return true
+}
+
+fn get_pwd() -> String{
+    env::current_dir().unwrap().into_os_string().into_string().unwrap()
+}
+fn print_pwd() {
+    let pwd = get_pwd();
+    let last_index = pwd.rfind('/').unwrap();
+    print!("{} ", &pwd[last_index+1..]);
+}
+
+fn cd(args: &Vec<&str>) {
+    if args.is_empty() {
+        let home_dir = dirs::home_dir().unwrap();
+        let _=env::set_current_dir(&home_dir.as_path());
+        return
+    } else if args.len() > 1 {
+        println!("cd: too many arguments");
+        return
+    }
+    let dir_path: &str = args[0];
+    let path = Path::new(&dir_path);
+    let dir_set_result = env::set_current_dir(&path);
+
+    match dir_set_result {
+        Ok(dir) => dir,
+        Err(err) => {
+            println!("cd: no such file or directory");
+            return
+        },
+    };
+}
+
+/**
+ * Exists because normal cat does not make a new line if new line is not included at the end of the file
+ */
+fn cat(args: &Vec<&str>) {
+    // let pwd = get_pwd();
+    let path = args[0];
+    if let Ok(mut file) = File::open(path) {
+
+        let mut buffer: [u8; 1] = [0; 1];
+        loop {
+            match file.read(&mut buffer) {
+                Ok(0) => break, // EOF
+                Ok(_) => {
+                    let ascii_char = buffer[0] as char;
+                    print!("{ascii_char}");
+                },
+                Err(e) => println!("{:?}", e),
+            }
+        }
+
+        let percent_color: Style = Color::Black.on(Color::White);
+        if buffer[0] as char != '\n' {
+            println!("{}", percent_color.paint("%"))  // put a % if no new line
+        }
+
+    } else {
+        println!("cat: No such file or directory: {path}");
+    }
+}
+
+fn split_input<'a>(tokens: &'a mut Vec<&str>) -> (Vec<&'a str>, Option<String>) {
+    let mut part = Vec::new();
+    let mut part_index = 0;
+    let mut separator: Option<String> = None;
+
+    for token in &mut *tokens {
+        part_index+=1;
+        if *token == "&&" {
+            separator = Some("&&".to_string());
+            break;
+        } else if *token == "||" {
+            separator = Some("||".to_string());
+            break;
+        }
+        part.push(*token);
+    }
+    for i in 0..part_index {
+        tokens.remove(0);
+    }
+    return (part, separator);
 }
